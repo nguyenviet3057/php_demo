107f97667eb95d28f749987cdc2cbe96aeac9255
Viet <nguyenviet3057@gmail.com>
2021-09-14 20:08:56 +0700

    fixed scrolling and clicking panel

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-var check = 0;
+var x = 0;
 var y = $("#q2_t").offset().top + parseInt($("#pre_bg").css("height"));
 var z = document.getElementById("welcome").offsetHeight+document.getElementById("pre").offsetHeight+230;
 $("#home_p").css("height",z+"px");
 $("#information").scroll(function(){
-    var check = 0;
-    var pre_offset = $("#pre").offset();
-    var x = $(this).scrollTop();
+    x = $(this).scrollTop();
     var f_size = Math.min(24+0.04*x, 48);
     var o_welcome = 1-0.001*x;
     var p_size = Math.min(0.2*x,150);
     var w_q2bg = Math.min(x/160*0.1*x, 100);
     var o_q2bg = 0;
     $("#bg").offset({top:0});
-    if (x < 550){
+    if (x < 550 && check == 0){
         $("#bg").css({"opacity":Math.min(0.0015*x, 0.75)});
+        $(".reference,#header").css({"filter":"invert("+Math.min(0.002*x, 1)+")"});
     };
     if (x>=200){
         o_q2bg = 0.0025*(x-200);
@@ -28,7 +27,6 @@ $("#information").scroll(function(){
     if (document.getElementById("q2_bg").style.width == "100%"){
         $("#preview").css({"transition":"1s linear","height":"36vw"});
         $("#q2_t").css({"color":"white"});
-        $(".reference,#header").css("filter","invert(100%)");
         if (document.getElementById("q2_t").style.color == "white"){
                 $("#q3").css("visibility","visible");
         };
@@ -41,57 +39,61 @@ $("#information").scroll(function(){
     };
     if ($("#preview").offset().top <= 100 || document.getElementById("preview").style.height != "36vw"){
         document.getElementById("video").pause();
-        $(".reference,#header").css("filter","none");
     }else if ($("#preview").offset().top <= 200){
         document.getElementById("video").play();
     };
-    if ($("#q2_t").offset().top <= 0 && x>y){
+    if ($("#q2_t").offset().top <= 0 && x>y && check == 0){
         $("#bg").css({"opacity":Math.max(0.75-0.0015*(x-y), 0)});
+        $(".reference,#header").css({"filter":"invert("+Math.max(1-0.002*(x-y), 0)+")"});
     };
 });
 $("#video").click(function(){
     document.getElementById("video").play();
 });
-$("#home").click(function(){
+$("#h_r").click(function(){
     $("#bg").css({"visibility":"hidden"});
-    $(".reference,#header").css("transition-delay","0.25s"); // = transition-duration to prevent scrollIntoview bad transition
     document.getElementById("home_p").scrollIntoView({behavior:"smooth"});
     setTimeout(function(){
         $("#bg").css({"visibility":"visible"});
-        $(".reference,#header").css("transition-delay","0s");
     },750);
 });
-$("#get_info").click(function(){
+$("#i_r").click(function(){
     $("#bg").css({"visibility":"hidden"});
-    $(".reference,#header").css("transition-delay","0.25s"); // = transition-duration to prevent scrollIntoview bad transition
     document.getElementById("info_p").scrollIntoView({behavior:"smooth"});
     setTimeout(function(){
         $("#bg").css({"visibility":"visible"});
         $("#q2").css({"visibility":"visible"});
-        $(".reference,#header").css("transition-delay","0s");
     },750);
 });
-$("#support").click(function(){
-    alert($("#home_p").css("height"));
+$("#h_r").mouseover(function(){
+    $(this).css("backdrop-filter","invert(1)");
 });
-// $("#contact").click(function(){
-//     alert(document.getElementById("q2_t").style.color);
-// });
-$("#home_r").mouseover(function(){
-    $(this).css("filter","invert(100%)");
+$("#i_r").mouseover(function(){
+    $(this).css("backdrop-filter","invert(1)");
 });
-$("#info_r").mouseover(function(){
-    $(this).css("filter","invert(100%)");
+$("#c_r").mouseover(function(){
+    $(this).css("backdrop-filter","invert(1)");
 });
-$("#contact_r").mouseover(function(){
-    $(this).css("filter","invert(100%)");
+$("#s_r").mouseover(function(){
+    $(this).css("backdrop-filter","invert(1)");
+
 });
-$("#support_r").mouseover(function(){
-    $(this).css("filter","invert(100%)");
+$("#h_r").mouseout(function(){
+    $(this).css("backdrop-filter","invert(0)");
+
 });
-$(".reference").mouseout(function(){
-    $(this).css("filter","none");
+$("#i_r").mouseout(function(){
+    $(this).css("backdrop-filter","invert(0)");
 });
-$("#contact").click(function(){
+$("#c_r").mouseout(function(){
+    $(this).css("backdrop-filter","invert(0)");
+
+});
+$("#s_r").mouseout(function(){
+    $(this).css("backdrop-filter","invert(0)");
+});
+$("#c_r").click(function(){
     document.getElementById("contact_p").scrollIntoView({behavior:"smooth"});
+});
+$("#s_r").click(function(){
 });
